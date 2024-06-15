529b347eeb739d8cff39de1d8113264c53e27946
yaniv-simmer <simmeryaniv@gmail.com>
2024-06-15 17:20:04 +0300

    Added exception handling (bonus).and handled unusual whitespaces

--- gpt_explainer/scripts/pptx_text_extractor.py
+++ gpt_explainer/scripts/pptx_text_extractor.py
@@ -1,24 +1,30 @@
-from typing import Generator, List
+from typing import Generator
 from pptx import Presentation
-
+from pptx.exc import PackageNotFoundError
 
 class PptxParser:
     """
     A class used to parse PPTX files and extract text from slides.
     """
 
-    def __init__(self, file_path: str):
+    def __init__(self, pptx_file_path: str):
         """
         Initialize PptxParser with a PPTX file.
 
         Parameters:
             file_path (str): The path to the PPTX file.
+        Raises:
+            FileNotFoundError: If the file is not found.
         """
-        self.presentation = Presentation(file_path)
+        try:
+            self.presentation = Presentation(pptx_file_path)
+        except (PackageNotFoundError):
+            raise FileNotFoundError(f"File not found: {pptx_file_path}")
+        
 
     def extract_text_from_single_slide(self, slide) -> str:
         """
-        Extracts and returns all text from a single slide.
+        Extracts and returns all text from a single slide formatted as a string.
 
         Parameters:
             slide: A slide object from a PPTX file.
@@ -26,17 +32,20 @@ class PptxParser:
         Returns:
             str: A string representing the text from the slide.
         """
-        return '\n'.join([shape.text for shape in slide.shapes if hasattr(shape, "text")])
-
+        return '\n'.join([' '.join(shape.text.split()) for shape in slide.shapes if hasattr(shape, "text")])
+    
     def generate_text_from_slides(self) -> Generator[str, None, None]:
         """
         Extracts and yields all text from the slides in a PPTX file.
 
         Yields:
-            str: A string representing the text from a slide.        
+            str: A string representing the text from a slide. If an error occurs, the error message is yielded.        
         """
-        for slide in self.presentation.slides:
-            slide_text = self.extract_text_from_single_slide(slide)
-            if slide_text:
-                yield slide_text
+        for i, slide in enumerate(self.presentation.slides):
+            try:
+                slide_text = self.extract_text_from_single_slide(slide)
+                if slide_text:
+                    yield slide_text
+            except Exception as e:
+                yield f"Error processing slide {i+1}: {str(e)}"
 
