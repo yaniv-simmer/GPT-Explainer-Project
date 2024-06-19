1eb97512fc9404301b1c1dc3e05c2c3e9c57ced3
yaniv-simmer <simmeryaniv@gmail.com>
2024-06-19 11:43:19 +0300

    Added docstrings

--- gpt_explainer/tests/sys_test.py
+++ gpt_explainer/tests/sys_test.py
@@ -3,53 +3,36 @@ import pytest
 import subprocess
 import sys
 
-input_presentation = os.path.abspath(os.path.join('..\presentations', 'Chapter_6_5-6.pptx'))
-# print the working dir
-
-output_file = os.path.abspath(os.path.join(os.getcwd(),'output', 'Chapter_6_5-6.json'))
+INPUT_FILE = os.path.abspath(os.path.join('..\presentations', 'Chapter_6_5-6.pptx'))
+OUTPUT_FILE = os.path.abspath(os.path.join(os.getcwd(),'output', 'Chapter_6_5-6.json'))
 
 
 def teardown_module():
-    #output_file_ = os.path.abspath(os.path.join('..\output', 'Chapter_6_5-6.json'))
-    if os.path.exists(output_file):
-        os.remove(output_file)
+    """
+    This function is called after the module is done executing.
+    It checks if the output file exists and removes it if it does.
+    """
+    if os.path.exists(OUTPUT_FILE):
+        os.remove(OUTPUT_FILE)
 
 
 def test_system():
-    # Clean up the output file before the test
-    if os.path.exists(output_file):
-        os.remove(output_file)
+    """
+    This function tests the system by running the main script with the input presentation file.
+    It first checks if the output file exists and removes it if it does.
+    Then it runs the main script with the input presentation file as an argument.
+    It asserts that the script executed successfully by checking the return code.
+    Finally, it asserts that the output file was created.
+    """
+    if os.path.exists(OUTPUT_FILE):
+        os.remove(OUTPUT_FILE)
 
-    # Run the main script
     script_path = os.path.abspath(os.path.join('..\scripts', 'main.py'))
-    print('\nfffff\n'+script_path)
-    result = subprocess.run(['python3.10.exe',script_path, "--pptx_file_path", input_presentation], timeout=100)
+    result = subprocess.run(['python3',script_path, "--pptx_file_path", INPUT_FILE], timeout=250)
 
-    # Check the script executed without errors
     assert result.returncode == 0, "Script did not execute successfully"
 
-    # print ls
-    #print('\nllllllist\n'+os.listdir(os.path.abspath(os.path.join('..\output'))))
-    print()
-    print('\nrrrrr+ \n'+output_file)
-    assert os.path.exists(output_file), "Output file was not created"
+    assert os.path.exists(OUTPUT_FILE), "Output file was not created"
+    teardown_module()
 
-    
 
-# def test_system():
-#     '''
-#     Test the system as a whole
-#     '''
-#     input_presentation = INPUT_PRESENTATION
-#     output_file = EXPECTED_OUTPUT_FILE
-#     print(f"Input file: {input_presentation}")
-#     print(f"Expected output file: {output_file}")
-#
-#     w = subprocess.run(["python3.10.exe", r"..\scripts\main.py", "--pptx_file_path", input_presentation],
-#                        capture_output=True)
-#     print(f"Subprocess output: {w.stdout.decode()}")
-#
-#     assert os.path.exists(output_file), "Output file was not created"
-#
-#     if os.path.exists(output_file):
-#         os.remove(output_file)
