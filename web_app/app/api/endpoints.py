9c5dd16ce06d2ec14ba51072c4376a3007b85b5d
yaniv-simmer <simmeryaniv@gmail.com>
2024-07-01 22:10:46 +0300

    small changes

--- web_app/app/api/endpoints.py
+++ web_app/app/api/endpoints.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request, jsonify
 import os
 import uuid
-import hashlib
 from datetime import datetime
 import glob
 
@@ -16,6 +15,12 @@ os.makedirs(PROCESSED_FOLDER, exist_ok=True)
 
 @api_bp.route('/upload', methods=['POST'])
 def upload_file():
+    '''
+    Uploads a file to the server and returns a unique identifier (UID) for the file
+    
+    Returns:
+        - JSON response with the UID of the uploaded file
+    '''
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'}), 400
     file = request.files['file']
@@ -23,10 +28,7 @@ def upload_file():
         return jsonify({'error': 'No selected file'}), 400
     
     # Ensure UID is unique for each file
-    file_content = file.read()
-    uid = hashlib.sha256(file_content).hexdigest()
-    file.seek(0)  # Reset file pointer to the beginning
-    
+    uid = uuid.uuid4().hex    
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     filename = os.path.join(UPLOAD_FOLDER, f"{file.filename}_{timestamp}_{uid}")
     
@@ -38,11 +40,23 @@ def upload_file():
 
 @api_bp.route('/status/<uid>', methods=['GET'])
 def check_status(uid):
-    # Use glob to find the original file based on the UID
+    '''
+    Checks the status of a file upload job. If the job is done, the explanation is also returned
+
+    Parameters:
+        - uid (str): The unique identifier of the file upload job
+        
+    Returns:
+        - JSON response with the status of the job
+    '''
+    original_file_found = False
+    original_filename = ""
+    timestamp = ""
+    explanation = None
+    
     files = glob.glob(os.path.join(UPLOAD_FOLDER, f"*_{uid}"))
     
     if files:
-        # Assuming the first match is the correct file
         file_path = files[0]
         original_file_found = True
         parts = os.path.basename(file_path).rsplit('_', 2)
@@ -53,7 +67,6 @@ def check_status(uid):
     if not original_file_found:
         return jsonify({'status': 'not found'}), 404
     
-    # Check if the processed result file exists
     result_file = os.path.join(PROCESSED_FOLDER, f"{uid}_result.json")
     if os.path.exists(result_file):
         with open(result_file, 'r') as f:
@@ -68,12 +81,4 @@ def check_status(uid):
         'timestamp': timestamp if original_file_found else "",
         'explanation': explanation if original_file_found else None
     }), 200
-@api_bp.route('/result/<uid>', methods=['GET'])
-def get_result(uid):
-    # Placeholder for retrieving job result
-    result_file = os.path.join(PROCESSED_FOLDER, f"{uid}_result.json")
-    if os.path.exists(result_file):
-        with open(result_file, 'r') as f:
-            result = f.read()
-        return jsonify({'result': result}), 200
-    return jsonify({'error': 'Result not found'}), 404
+
